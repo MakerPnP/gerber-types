db19dc851662aa4f6f78f61b98f1a8ca5479a4ad
Dominic Clifton <me@dominicclifton.name>
2025-06-13 14:37:45 +0200

    Hide `PartialGerberCode` again.

--- src/lib.rs
+++ src/lib.rs
@@ -14,9 +14,8 @@
 //!
 //! - [`GerberCode`](trait.GerberCode.html) generates a full Gerber code line,
 //!   terminated with a newline character.
-//! - [`PartialGerberCode`](trait.PartialGerberCode.html) generates Gerber representation of a
+//! - `PartialGerberCode` (internal only) generates Gerber representation of a
 //!   value, but does not represent a full line of code.
-//!   This should only be used by developers implementing parsers. If you need it for another reason, please let us know.
 #![allow(clippy::new_without_default)]
 
 #[cfg(test)]
@@ -42,7 +41,7 @@ pub use crate::errors::*;
 pub use crate::extended_codes::*;
 pub use crate::function_codes::*;
 pub use crate::macros::*;
-pub use crate::traits::{GerberCode, PartialGerberCode};
+pub use crate::traits::GerberCode;
 pub use crate::types::*;
 
 // re-export some types
